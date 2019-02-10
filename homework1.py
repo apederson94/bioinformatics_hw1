9eeea4ff21d456d85174e91cb639f824c33a3201
Austin <amp845@nau.edu>
2019-02-09 23:15:41 -0700

    completed question 4 and made k_mer vastly more efficient.

--- homework1.py
+++ homework1.py
@@ -1,5 +1,8 @@
 #Created by Austin Pederson 02/08/2019 10:34
 import random
+import datetime
+import csv
+import re
 
 #Question 1
 def read_fasta(file_name, text=True):
@@ -19,9 +22,11 @@ def read_fasta(file_name, text=True):
             for tmp in content[1:]:
                 seq += tmp
             fasta_dict = {content[0]: bytes(seq)}
-                
+
     return fasta_dict
 
+
+#Question 2
 def calculate_gc_content(seq):
 
     if type(seq) == str:
@@ -37,35 +42,45 @@ def calculate_gc_content(seq):
 
     return (g + c)/(a + t + c + g)
 
+
+#Question 3
 def k_mer(sequence, k):
     mer_dict = {}
     seq_len = len(sequence)
 
-    for pos in range(seq_len):
+    for pos in range((seq_len-k)+1):
         mer = sequence[pos:pos+k]
-        if(mer_dict.get(mer)):
-            continue
+        if(not mer_dict.get(mer)):
+            mer_dict[mer] = 1
         else:
-            mer_dict[mer] = sequence.count(mer)
+            mer_dict[mer] += 1
 
     return mer_dict
 
-#TODO: this
+
+#Question 4
 def jaccard_dist(mer_dict1, mer_dict2):
-    union = []
-    intersection = []
+    union = {}
+    intersection = {}
+    int_count = 0
+    un_count = 0
+
     for mer in mer_dict1:
 
         if mer_dict2.get(mer):
-            intersection.append(mer)
+            count = min(mer_dict1[mer], mer_dict2[mer])
+            intersection[mer] = count
+            int_count += count
 
         if mer not in union:
-            union.append(mer)
+            count = max(mer_dict1[mer], mer_dict2[mer])
+            union[mer] = count
+            un_count += count
 
-    #print(union)
-    #print("\n")
-    #print(intersection)
-    
+    return int_count/un_count
+
+
+#Question 5
 def read_samples(sequence, mer_length, read_count):
     samples_dict = {}
     sample_len = len(sequence)
@@ -73,28 +88,75 @@ def read_samples(sequence, mer_length, read_count):
         pos = random.randint(0, sample_len)
         header = str(num + 1) + "|" + str(pos)
         samples_dict[header] = sequence[pos:pos+mer_length]
-    print(samples_dict)
+
 
 def main():
-    seq_003997_bin = read_fasta("NC_003997.fasta", False)
-    seq_003997_str = read_fasta("NC_003997.fasta")
     seq_004722_bin = read_fasta("NC_004722.fasta", False)
+    seq_003997_bin = read_fasta("NC_003997.fasta", False)
     seq_004722_str = read_fasta("NC_004722.fasta")
 
-    header_003997_bin = next(iter(seq_003997_bin.keys()))
-    header_003997_str = next(iter(seq_003997_str.keys()))
     header_004722_bin = next(iter(seq_004722_bin.keys()))
+    header_003997_bin = next(iter(seq_003997_bin.keys()))
     header_004722_str = next(iter(seq_004722_str.keys()))
-    
-    calculate_gc_content(seq_003997_bin[header_003997_bin])
 
     k1 = k_mer(seq_003997_bin[header_003997_bin], 4)
-    k2 = k_mer(seq_003997_str[header_003997_str], 4)
+    k2 = k_mer(seq_004722_bin[header_004722_bin], 4)
 
+    jaccard_dist(k1, k2)
 
-    #jaccard_dist(k1, k2)
+"""     #Question 6
 
-    #read_samples(seq_003997_bin[header_003997_bin], 45, 25)
+    #part1
+    with open("q6_p1.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, delimiter = ",")
+        writer.writerow(["sequence_name", "text", "replicate", "seconds"])
+        
+        for i in range(10):
+            tstart = datetime.datetime.now()
+            read_fasta("NC_004722.fasta", False)
+            t_bin = datetime.datetime.now() - tstart
+
+            tstart = datetime.datetime.now()
+            #read_fasta("NC_003997.fasta")
+            t_str = datetime.datetime.now() - tstart
+    
+            writer.writerow([header_004722_bin, False, i, t_bin])
+            writer.writerow([header_004722_str, True, i, t_str])
+
+    #part2
+    with open("q6_p2.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["sequence_name", "text", "replicate", "k"])
+        for k in [4, 8, 12]:
+            for i in range(10):
+                tstart = datetime.datetime.now()
+                k_mer(seq_004722_bin[header_004722_bin], k)
+                t_bin = datetime.datetime.now() - tstart
+
+                tstart = datetime.datetime.now()
+                k_mer(seq_004722_str[header_004722_str], k)
+                t_str = datetime.datetime.now() - tstart
+                
+                writer.writerow([header_004722_bin, False, i, t_bin, k])
+                writer.writerow([header_004722_str, True, i, t_str, k])
+            
+
+   #part3
+    with open("q6_p3.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["sequence_name", "text", "replicate", "mer_length", "read_count"])
+        for read_count in [100000, 500000, 1000000, 10000000, 25000000, 50000000]:
+            for i in range(10):
+                tstart = datetime.datetime.now()
+                read_samples(seq_004722_bin[header_004722_bin], 50, read_count)
+                t_bin = datetime.datetime.now() - tstart
+
+                tstart = datetime.datetime.now()
+                read_samples(seq_004722_str[header_004722_str], 50, read_count)
+                t_str = datetime.datetime.now() - tstart
+                
+                writer.writerow([header_004722_bin, False, i, t_bin, 50, read_count])
+                writer.writerow([header_004722_str, True, i, t_str, 50, read_count]) """
 
 
 
